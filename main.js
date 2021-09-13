b51dc64d82e88446dbed5f38f5dfabcd438ccd4e
ro-ed <robinedin94@gmail.com>
2021-09-13 18:09:14 +0200

    Update

--- main.js
+++ main.js
@@ -2,14 +2,21 @@
 
 Vue.component('website-title', 
 {
-    template: '<h1>Webbshopen</h1>'
+    template: '<div><h1>SCABLE</h1><p>™</p></div>'
 }
 )
 
 
 Vue.component('header-image', 
 {
-    template: '<img src="/images/background-unsplash.jpg" alt="Image not working">'
+    template: '<div><h1 style="color: #4F7942;">SUSTAINABLE.<br>COMFORTABLE.<br>AFFORDABLE.<br>We are SCABLE.</h1><img src="/images/background-unsplash.jpg" alt="Image not working"></div>'
+})
+
+Vue.component('header-icon-search', 
+{
+    template: '<div><svg id="showSearch" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">'
+    + '<path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>'
+    + '</svg></div>'
 })
 
 Vue.component('header-icon-profile', 
